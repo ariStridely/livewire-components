83f79cf234caca699f87e603e2fe2a0b2ac026d6
Aristotele Tufano <aristotele.tufano@stridely.com>
2022-10-17 16:16:32 +0200

    Extract choices alpinejs logic

--- resources/views/components/choices.blade.php
+++ resources/views/components/choices.blade.php
@@ -11,133 +11,12 @@
 <div
     wire:ignore
     x-cloak
-    x-data="{
-        choices: null,
-        search: '',
-        value: [],
-        multiple: Boolean({{ $multiple }}),
-        options: [],
-        remoteUrl: '{{ $remoteUrl }}',
-
-        extraConfig: {{ $extraConfig }},
-
-        init() {
-            this.$nextTick(() => {
-                
-                {{-- choices initialization --}}
-                this.choices = new Choices(this.$refs.select, {
-                    noChoicesText: () => {
-                        if (this.options.length >= 0 && this.search != '') {
-                            return 'No results found';
-                        }
-                        
-                        return 'Please select 1 or more character';
-                    },
-                    searchFloor: 0,
-                    searchChoices: this.multiple ? false : true,
-                    ...this.extraConfig
-                });
-
-                {{-- Define inner functions --}}
-                let refreshChoices = () => {
-                    let selection = this.multiple ? this.value : [this.value];
-                    
-                    this.choices.clearStore();
-                    this.choices.setChoices(
-                        this.options.map( ({ value, label }) => ({
-                            value,
-                            label,
-                            selected: selection.includes(value),
-                        }))
-                    );
-                }
-
-                let getOptions = async (q) => {
-                    let url = this.remoteUrl;
-                    let response = await fetch(url);
-                    let data = await response.json(); // read response body and parse as JSON
-
-                    return data;
-                }
-
-                {{-- Una tantum call refreshChoices --}}
-                refreshChoices();
-
-                {{-- Add event listeners --}}
-                if (this.remoteUrl) {
-                    this.$refs.select.addEventListener(
-                        'search', 
-                        _.debounce(async (event) => {
-                            {{-- chiamata api con termine di ricerca --}}
-                            let q = event.detail.value;
-                            this.search = q;
+    x-data="selectChoices(
+        '{{ $remoteUrl }}',
+        '{{ $id }}',
+        {{ $multiple ? 'true' : 'false' }} 
+    )"
     
-                            {{-- clear options list if canc search term --}}
-                            if (q == '') {
-                                let valueObject = this.choices.getValue();
-    
-                                if (!this.multiple) {
-                                    valueObject = valueObject ? [valueObject] : [];
-                                }
-    
-                                this.options = valueObject;
-    
-                                return;
-                            }
-    
-                            let data = await getOptions(q);
-    
-                            {{-- ci sono options selezionate? --}}
-                            let selectedOptions = this.choices.getValue();
-                            let selectedOptionsIds = this.choices.getValue(true);
-    
-                            if (!this.multiple) {
-                                selectedOptions = selectedOptions ? [selectedOptions] : [];
-                                selectedOptionsIds = selectedOptionsIds ? [selectedOptionsIds] : [];
-                            }
-    
-                            {{-- escludi da risultato api quelli già selezionati --}}
-                            let filtered = data.filter((model) => {
-                                return !selectedOptionsIds.includes(model.value);
-                            });
-    
-                            this.options = selectedOptions.concat(filtered);
-                        }, 1000)
-                    );
-                }
-
-                if (this.remoteUrl) {
-                    this.$refs.select.addEventListener('change', () => {
-                        this.value = this.choices.getValue(true) || null;
-                        let valueObject = this.choices.getValue();
-    
-                        if (!this.multiple) {
-                            valueObject = valueObject ? [valueObject] : [];
-                        }
-    
-                        this.search = '';
-                        this.options = valueObject;
-                    });
-                } else {
-                    this.$refs.select.addEventListener('change', () => {
-                        this.value = this.choices.getValue(true) || null;
-                    });
-                }
-
-                {{-- Add watchers --}}
-                this.$watch('value', () => refreshChoices());
-                this.$watch('options', () => refreshChoices());
-                
-            });
-        },
-
-        {{-- Add AlpineJS function listeners --}}
-        updateOptions(event) {
-            if (event.detail.ref != '{{ $id }}') return;
-
-            this.options = event.detail.options;
-        },
-    }"
     x-on:update-choices-options.window="updateOptions($event);"
 >
     <select 
@@ -160,27 +39,141 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
         
     @endpush
-
-    @push('scripts')
-        <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>        
-
-        <script>
-            // async function getOptions() {
-            //     let url = 'http://livewire-components.test/api/users';
-            //     let response = await fetch(url);
-    
-            //     let commits = await response.json(); // read response body and parse as JSON
-    
-            //     console.log(commits);
-            // }
-    
-    
-            // el.addEventListener("search", _.debounce(async (event) => {
-            //         let data = await getOptions(`${url}?q=${event.detail.value}`);
-    
-            //         select.setChoices(data, "value", "label", false);
-            //     }, 1000)
-            // );
-        </script>
-    @endpush
 @endonce
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>        
+
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('selectChoices', (remoteUrl = '', id, multiple = false) => ({
+                choices: null,
+                search: '',
+                value: [],
+                multiple: Boolean(multiple),
+                options: [],
+                remoteUrl: remoteUrl,
+                id: id,
+
+                extraConfig: {{ $extraConfig }},
+
+                init() {
+                    this.$nextTick(() => {
+
+                        //  choices initialization
+                        this.choices = new Choices(this.$refs.select, {
+                            noChoicesText: () => {
+                                if (this.options.length >= 0 && this.search != '') {
+                                    return 'No results found';
+                                }
+
+                                return 'Please select 1 or more character';
+                            },
+                            searchFloor: 0,
+                            searchChoices: this.multiple ? false : true,
+                            ...this.extraConfig
+                        });
+
+                        //  Define inner functions
+                        let refreshChoices = () => {
+                            let selection = this.multiple ? this.value : [this.value];
+
+                            this.choices.clearStore();
+                            this.choices.setChoices(
+                                this.options.map( ({ value, label }) => ({
+                                    value,
+                                    label,
+                                    selected: selection.includes(value),
+                                }))
+                            );
+                        }
+
+                        let getOptions = async (q) => {
+                            let url = this.remoteUrl;
+                            let response = await fetch(url);
+                            let data = await response.json(); // read response body and parse as JSON
+
+                            return data;
+                        }
+
+                        //  Una tantum call refreshChoices
+                        refreshChoices();
+
+                        //  Add event listeners
+                        if (this.remoteUrl) {
+                            this.$refs.select.addEventListener(
+                                'search', 
+                                _.debounce(async (event) => {
+                                    //  chiamata api con termine di ricerca
+                                    let q = event.detail.value;
+                                    this.search = q;
+
+                                    //  clear options list if canc search term
+                                    if (q == '') {
+                                        let valueObject = this.choices.getValue();
+
+                                        if (!this.multiple) {
+                                            valueObject = valueObject ? [valueObject] : [];
+                                        }
+
+                                        this.options = valueObject;
+
+                                        return;
+                                    }
+
+                                    let data = await getOptions(q);
+
+                                    //  ci sono options selezionate?
+                                    let selectedOptions = this.choices.getValue();
+                                    let selectedOptionsIds = this.choices.getValue(true);
+
+                                    if (!this.multiple) {
+                                        selectedOptions = selectedOptions ? [selectedOptions] : [];
+                                        selectedOptionsIds = selectedOptionsIds ? [selectedOptionsIds] : [];
+                                    }
+
+                                    //  escludi da risultato api quelli già selezionati
+                                    let filtered = data.filter((model) => {
+                                        return !selectedOptionsIds.includes(model.value);
+                                    });
+
+                                    this.options = selectedOptions.concat(filtered);
+                                }, 1000)
+                            );
+                        }
+
+                        if (this.remoteUrl) {
+                            this.$refs.select.addEventListener('change', () => {
+                                this.value = this.choices.getValue(true) || null;
+                                let valueObject = this.choices.getValue();
+
+                                if (!this.multiple) {
+                                    valueObject = valueObject ? [valueObject] : [];
+                                }
+
+                                this.search = '';
+                                this.options = valueObject;
+                            });
+                        } else {
+                            this.$refs.select.addEventListener('change', () => {
+                                this.value = this.choices.getValue(true) || null;
+                            });
+                        }
+
+                        //  Add watchers
+                        this.$watch('value', () => refreshChoices());
+                        this.$watch('options', () => refreshChoices());
+
+                    });
+                },
+
+                //  Add AlpineJS function listeners
+                updateOptions(event) {
+                    if (event.detail.ref != id) return;
+
+                    this.options = event.detail.options;
+                },
+            }))
+        })
+    </script>
+@endpush
